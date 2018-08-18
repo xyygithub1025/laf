xyygithub1025/laf

commit 30481c99dcd37a1abf2f098c49a3822f4c5c6153

macOS: Improve window resizing handling
Now we create just one Event::ResizeDisplay and provide a better live
resizing experience (resizing the surface).

os/skia/skia_display.cpp
@@ -54,18 +54,27 @@ void SkiaDisplay::resetSkiaSurface()
 
 void SkiaDisplay::resize(const gfx::Size& size)
 {
-  Event ev;
-  ev.setType(Event::ResizeDisplay);
-  ev.setDisplay(this);
-  os::queue_event(ev);
-
   if (!m_initialized || m_customSurface)
     return;
 
+  gfx::Size newSize(size.w / m_window.scale(),
+                    size.h / m_window.scale());
+
+  if (m_initialized && m_surface &&
+      m_surface->width() == newSize.w &&
+      m_surface->height() == newSize.h) {
+    return;
+  }
+
   m_surface->dispose();
   m_surface = new SkiaSurface;
-  m_surface->create(size.w / m_window.scale(),
-                    size.h / m_window.scale());
+  m_surface->create(newSize.w, newSize.h);
+
+#if !defined(__APPLE__)
+  Event ev;
+  ev.setType(Event::ResizeDisplay);
+  ev.setDisplay(this);
+#endif
 }
 
 void SkiaDisplay::dispose()

os/osx/window.h
@@ -27,6 +27,8 @@ public:
   virtual void onResize(const gfx::Size& size) = 0;
   virtual void onDrawRect(const gfx::Rect& rect) = 0;
   virtual void onWindowChanged() = 0;
+  virtual void onStartResizing() = 0;
+  virtual void onEndResizing() = 0;
 };
 
 @class OSXWindowDelegate;

os/osx/window_delegate.h
@@ -34,8 +34,14 @@ class OSXWindowImpl;
   m_impl->onClose();
 }
 
-- (void)windowDidResize:(NSNotification*)notification
+- (void)windowWillStartLiveResize:(NSNotification*)notification
 {
+  m_impl->onStartResizing();
+}
+
+- (void)windowDidEndLiveResize:(NSNotification*)notification
+{
+  m_impl->onEndResizing();
 }
 
 - (void)windowDidMiniaturize:(NSNotification*)notification

os/skia/skia_window_osx.mm
@@ -45,6 +45,7 @@ public:
        int width, int height, int scale)
     : m_display(display)
     , m_backend(Backend::NONE)
+    , m_oldSurface(nullptr)
 #if SK_SUPPORT_GPU
     , m_nsGL(nil)
     , m_skSurface(nullptr)
@@ -166,6 +167,14 @@ public:
 #endif
 
     m_display->resize(size);
+
+    if (m_oldSurface) {
+      SkiaSurface* displaySurface = static_cast<SkiaSurface*>(m_display->getSurface());
+      displaySurface->drawSurface(
+        m_oldSurface,
+        gfx::Rect(0, 0, m_oldSurface->width(), m_oldSurface->height()),
+        gfx::Rect(0, 0, displaySurface->width(), displaySurface->height()));
+    }
   }
 
   void onDrawRect(const gfx::Rect& rect) override {
@@ -190,6 +199,31 @@ public:
     if (m_nsGL)
       [m_nsGL setView:[m_window contentView]];
 #endif
+  }
+
+  void onStartResizing() override {
+    ASSERT(!m_oldSurface);
+
+    const SkiaSurface* surface =
+      static_cast<SkiaSurface*>(m_display->getSurface());
+
+    m_oldSurface = new SkiaSurface;
+    m_oldSurface->create(surface->width(),
+                         surface->height());
+    m_oldSurface->drawSurface(surface, 0, 0);
+  }
+
+  void onEndResizing() override {
+    if (m_oldSurface) {
+      m_oldSurface->dispose();
+      m_oldSurface = nullptr;
+    }
+
+    // Generate the resizing display event for the user.
+    Event ev;
+    ev.setType(Event::ResizeDisplay);
+    ev.setDisplay(m_display);
+    os::queue_event(ev);
   }
 
 private:
@@ -368,6 +402,10 @@ private:
   Backend m_backend;
   bool m_closing;
   OSXWindow* m_window;
+
+  // Surface used for live resizing.
+  SkiaSurface* m_oldSurface;
+
 #if SK_SUPPORT_GPU
   std::unique_ptr<GLContext> m_glCtx;
   sk_sp<const GrGLInterface> m_glInterfaces;

commit dfc7872e420406025393a1bbb80cf0da56a3d3c2

Add missing queue_event() for ResizeDisplay (regression introduced in 30481c99dcd37a1abf2f098c49a3822f4c5c6153)

os/skia/skia_display.cpp
@@ -74,6 +74,7 @@ void SkiaDisplay::resize(const gfx::Size& size)
   Event ev;
   ev.setType(Event::ResizeDisplay);
   ev.setDisplay(this);
+  os::queue_event(ev);
 #endif
 }
 