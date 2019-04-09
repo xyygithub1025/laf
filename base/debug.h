97fc7297708c8a037aae2cf75481bee6c780001d
David Capello <david@igarastudio.com>
2019-04-09 16:32:15 -0300

    Add TRACEARGS(...) to print the given arguments

--- base/debug.h
+++ base/debug.h
@@ -1,4 +1,5 @@
 // LAF Base Library
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (c) 2001-2016 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -35,6 +36,40 @@ void base_trace(const char* msg, ...);
 #else
   #define ASSERT(condition)
   #define TRACE(...)
+#endif
+
+#if defined(__cplusplus) && defined(_DEBUG)
+  #include <sstream>
+  #include <string>
+
+  template<typename Arg>
+  void base_args_to_string_step(std::stringstream& s, Arg&& arg) {
+    s << arg;
+  }
+
+  template<typename Arg, typename... Args>
+  void base_args_to_string_step(std::stringstream& s, Arg&& arg, Args&&... args) {
+    s << arg << ' ';
+    base_args_to_string_step<Args...>(s, std::forward<Args>(args)...);
+  }
+
+  template<typename... Args>
+  std::string base_args_to_string(Args&&... args) {
+    std::stringstream s;
+    base_args_to_string_step(s, std::forward<Args>(args)...);
+    return s.str();
+  }
+
+  template<typename... Args>
+  void base_trace_args(Args&&... args) {
+    std::string s = base_args_to_string(std::forward<Args>(args)...);
+    s.push_back('\n');
+    base_trace(s.c_str());
+  }
+
+  #define TRACEARGS base_trace_args
+#else
+  #define TRACEARGS(...)
 #endif
 
 #endif
