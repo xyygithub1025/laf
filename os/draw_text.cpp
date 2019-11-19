xyygithub1025/laf

commit f0b5f1a5ca475659f6451a79f6973948106efa9d

Use bitmap pixel mode instead of antialias property to draw glyphs

os/draw_text.cpp
@@ -103,7 +103,6 @@ retry:;
 
     case FontType::kTrueType: {
       FreeTypeFont* ttFont = static_cast<FreeTypeFont*>(font);
-      bool antialias = ttFont->face().antialias();
       int fg_alpha = gfx::geta(fg);
 
       gfx::Rect clipBounds;
@@ -157,10 +156,10 @@ retry:;
 
               // Skip first clipped pixels
               for (int u=0; u<dstBounds.x-origDstBounds.x; ++u) {
-                if (antialias) {
+                if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
                   ++p;
                 }
-                else {
+                else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
                   if (bit == 8) {
                     bit = 0;
                     ++p;
@@ -172,10 +171,12 @@ retry:;
                 ASSERT(clipBounds.contains(gfx::Point(dst_x, dst_y)));
 
                 int alpha;
-                if (antialias) {
-                  alpha = *(p++);
+                if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
+                  // alpha = *(p++);
+                  alpha = 0;
+                  ++p;
                 }
-                else {
+                else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
                   alpha = ((*p) & (1 << (7 - (bit++))) ? 255: 0);
                   if (bit == 8) {
                     bit = 0;

commit c4b8402aeafa50c37122a63223ddb72218f11919

Fix bug drawing antialiased fonts (regression from f0b5f1a5ca475659f6451a79f6973948106efa9d)
The change was made just for testing purposes but it passed to the
official release.

os/draw_text.cpp
@@ -172,9 +172,7 @@ retry:;
 
                 int alpha;
                 if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
-                  // alpha = *(p++);
-                  alpha = 0;
-                  ++p;
+                  alpha = *(p++);
                 }
                 else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
                   alpha = ((*p) & (1 << (7 - (bit++))) ? 255: 0);