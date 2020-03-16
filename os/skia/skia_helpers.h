xyygithub1025/laf

commit dd9f578f924f1f8901920b3999350bfe2aaee29a

Add Paint::Style, antialias, drawCircle(), and remove fillRect()

os/skia/skia_helpers.h
@@ -1,0 +1,46 @@
+// LAF OS Library
+// Copyright (c) 2019  Igara Studio S.A.
+//
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifndef OS_SKIA_SKIA_HELPERS_INCLUDED
+#define OS_SKIA_SKIA_HELPERS_INCLUDED
+#pragma once
+
+#include "gfx/color.h"
+#include "gfx/rect.h"
+#include "os/paint.h"
+
+#include "SkColor.h"
+#include "SkPaint.h"
+#include "SkRect.h"
+
+namespace os {
+
+inline SkColor to_skia(gfx::Color c) {
+  return SkColorSetARGB(gfx::geta(c), gfx::getr(c), gfx::getg(c), gfx::getb(c));
+}
+
+inline SkIRect to_skia(const gfx::Rect& rc) {
+  return SkIRect::MakeXYWH(rc.x, rc.y, rc.w, rc.h);
+}
+
+inline SkRect to_skia(const gfx::RectF& rc) {
+  return SkRect::MakeXYWH(rc.x, rc.y, rc.w, rc.h);
+}
+
+inline void to_skia(const Paint& paint, SkPaint& skPaint) {
+  static_assert((int)SkPaint::kFill_Style == (int)Paint::Fill &&
+                (int)SkPaint::kStroke_Style == (int)Paint::Stroke &&
+                (int)SkPaint::kStrokeAndFill_Style == (int)Paint::StrokeAndFill,
+                "Paint styles don't match with Skia");
+
+  skPaint.setColor(to_skia(paint.color()));
+  skPaint.setStyle((SkPaint::Style)paint.style());
+  skPaint.setAntiAlias(paint.antialias());
+}
+
+} // namespace os
+
+#endif

os/skia/skia_surface.cpp
@@ -1,5 +1,5 @@
 // LAF OS Library
-// Copyright (c) 2018  Igara Studio S.A.
+// Copyright (c) 2018-2019  Igara Studio S.A.
 // Copyright (C) 2016-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -12,6 +12,7 @@
 #include "os/skia/skia_surface.h"
 
 #include "base/file_handle.h"
+#include "os/skia/skia_helpers.h"
 
 #include "SkCodec.h"
 #include "SkPixelRef.h"
@@ -20,6 +21,29 @@
 #include <memory>
 
 namespace os {
+
+void SkiaSurface::drawLine(const float x0, const float y0,
+                           const float x1, const float y1,
+                           const Paint& paint)
+{
+  to_skia(paint, m_paint);
+  m_canvas->drawLine(x0, y0, x1, y1, m_paint);
+}
+
+void SkiaSurface::drawRect(const gfx::RectF& rc,
+                           const Paint& paint)
+{
+  to_skia(paint, m_paint);
+  m_canvas->drawRect(to_skia(rc), m_paint);
+}
+
+void SkiaSurface::drawCircle(const float cx, const float cy,
+                             const float radius,
+                             const Paint& paint)
+{
+  to_skia(paint, m_paint);
+  m_canvas->drawCircle(cx, cy, radius, m_paint);
+}
 
 // static
 Surface* SkiaSurface::loadSurface(const char* filename)

examples/allevents.cpp
@@ -125,19 +125,26 @@ private:
     os::SurfaceLock lock(surface);
     const gfx::Rect rc = surface->bounds();
 
+    os::Paint p;
+    p.style(os::Paint::Fill);
+    p.color(gfx::rgba(0, 0, 0, 8));
+
     // Scroll old lines
     int i;
     if (m_textLog.size() >= m_maxlines) {
       int h = m_lineHeight*newlines;
       surface->scrollTo(rc, 0, -h);
-      surface->fillRect(gfx::rgba(0, 0, 0, 8), gfx::Rect(rc.x, rc.y, rc.w, rc.h-h));
-      surface->fillRect(gfx::rgba(0, 0, 0), gfx::Rect(rc.x, rc.y+rc.h-h, rc.w, h));
+
+      surface->drawRect(gfx::Rect(rc.x, rc.y, rc.w, rc.h-h), p);
+      p.color(gfx::rgba(0, 0, 0));
+      surface->drawRect(gfx::Rect(rc.x, rc.y+rc.h-h, rc.w, h), p);
+
       i = (m_textLog.size()-newlines);
     }
     // First lines without scroll
     else {
       i = m_oldLogSize;
-      surface->fillRect(gfx::rgba(0, 0, 0, 8), gfx::Rect(rc.x, rc.y, rc.w, i*m_lineHeight));
+      surface->drawRect(gfx::Rect(rc.x, rc.y, rc.w, i*m_lineHeight), p);
     }
 
     os::Paint paint;
@@ -147,11 +154,9 @@ private:
                     gfx::Point(0, (1+i)*m_lineHeight), &paint);
 
     gfx::Rgb rgb(gfx::Hsv(m_hue, 1.0, 1.0));
-    surface->fillRect(gfx::rgba(rgb.red(), rgb.green(), rgb.blue()),
-                      gfx::Rect(m_mousePos.x-int(m_brushSize/2),
-                                m_mousePos.y-int(m_brushSize/2),
-                                int(m_brushSize),
-                                int(m_brushSize)));
+    paint.color(gfx::rgba(rgb.red(), rgb.green(), rgb.blue()));
+    paint.antialias(true);
+    surface->drawCircle(m_mousePos.x, m_mousePos.y, m_brushSize, paint);
 
     // Invalidates the whole display to show it on the screen.
     m_display->invalidateRegion(gfx::Region(rc));

examples/complextextlayout.cpp
@@ -11,10 +11,13 @@ void draw_display(os::Display* display)
   os::Surface* surface = display->getSurface();
   os::SurfaceLock lock(surface);
   const gfx::Rect rc = surface->bounds();
-  surface->fillRect(gfx::rgba(0, 0, 0), rc);
 
-  os::Paint paint;
-  paint.color(gfx::rgba(255, 255, 255));
+  os::Paint p;
+  p.color(gfx::rgba(0, 0, 0));
+  p.style(os::Paint::Fill);
+  surface->drawRect(rc, p);
+
+  p.color(gfx::rgba(255, 255, 255));
 
   const wchar_t* lines[] = { L"English",
                              L"Русский язык", // Russian
@@ -23,12 +26,12 @@ void draw_display(os::Display* display)
                              L"한국어",       // Korean
                              L"العَرَبِيَّة‎"  };     // Arabic
 
-  gfx::Point pt(0, 0);
+  gfx::Point pos(0, 0);
   for (auto line : lines) {
     os::draw_text_with_shaper(
-      surface, nullptr, base::to_utf8(line), pt, &paint);
-    pt.x += 0;
-    pt.y += 32;
+      surface, nullptr, base::to_utf8(line), pos, &p);
+    pos.x += 0;
+    pos.y += 32;
   }
 
   // Invalidates the whole display to show it on the screen.

examples/helloworld.cpp
@@ -11,15 +11,18 @@ void draw_display(os::Display* display)
   os::Surface* surface = display->getSurface();
   os::SurfaceLock lock(surface);
   const gfx::Rect rc = surface->bounds();
-  surface->fillRect(gfx::rgba(0, 0, 0), rc);
-  surface->drawLine(gfx::rgba(255, 0, 0), gfx::Point(0     , 0), gfx::Point(  rc.w, rc.h));
-  surface->drawLine(gfx::rgba(0, 128, 0), gfx::Point(rc.w/2, 0), gfx::Point(rc.w/2, rc.h));
-  surface->drawLine(gfx::rgba(0, 0, 255), gfx::Point(rc.w  , 0), gfx::Point(     0, rc.h));
 
-  os::Paint paint;
-  paint.color(gfx::rgba(255, 255, 255));
+  os::Paint p;
+  p.color(gfx::rgba(0, 0, 0));
+  p.style(os::Paint::Fill);
+  surface->drawRect(rc, p);
+
+  p.color(gfx::rgba(255, 0, 0)); surface->drawLine(0     , 0,   rc.w, rc.h, p);
+  p.color(gfx::rgba(0, 128, 0)); surface->drawLine(rc.w/2, 0, rc.w/2, rc.h, p);
+  p.color(gfx::rgba(0, 0, 255)); surface->drawLine(rc.w  , 0,      0, rc.h, p);
+  p.color(gfx::rgba(255, 255, 255));
   os::draw_text(surface, nullptr, "Hello World", rc.center(),
-                &paint, os::TextAlign::Center);
+                &p, os::TextAlign::Center);
 
   // Invalidates the whole display to show it on the screen.
   display->invalidateRegion(gfx::Region(rc));
@@ -86,6 +89,9 @@ int app_main(int argc, char* argv[])
             // Set scale
             display->setScale(1 + (int)(ev.scancode() - os::kKey1));
             redraw = true;
+            break;
+          default:
+            // Do nothing
             break;
         }
         break;

os/CMakeLists.txt
@@ -52,6 +52,9 @@ endif()
 # Skia backend
 
 if(LAF_BACKEND STREQUAL "skia")
+  if(APPLE)
+    add_definitions(-Wno-c++14-extensions)
+  endif()
   list(APPEND LAF_OS_SOURCES
     skia/os.cpp
     skia/resize_surface.cpp

os/paint.h
@@ -19,6 +19,13 @@ namespace os {
       kNineWithoutCenter = 1,
       kCenterAlign = 2,
       kEndAlign = 4,
+      kAntialias = 8,
+    };
+
+    enum Style {
+      Fill,
+      Stroke,
+      StrokeAndFill,
     };
 
     Flags flags() const { return m_flags; }
@@ -26,15 +33,30 @@ namespace os {
     void setFlags(const Flags flags) {
       m_flags = Flags(int(m_flags) | int(flags));
     }
+    void resetFlags(const Flags flags) {
+      m_flags = Flags(int(m_flags) & ~int(flags));
+    }
     bool hasFlags(const Flags flags) const {
       return (int(m_flags) & int(flags)) == int(flags);
     }
+
+    bool antialias() const { return hasFlags(kAntialias); }
+    void antialias(const bool state) {
+      if (state)
+        return setFlags(kAntialias);
+      else
+        return resetFlags(kAntialias);
+    }
+
+    Style style() const { return m_style; }
+    void style(const Style style) { m_style = style; }
 
     gfx::Color color() const { return m_color; }
     void color(const gfx::Color color) { m_color = color; }
 
   private:
     Flags m_flags = kNone;
+    Style m_style = Fill;
     gfx::Color m_color = gfx::ColorNone;
   };
 

os/skia/skia_draw_text.cpp
@@ -10,6 +10,7 @@
 
 #include "os/draw_text.h"
 #include "os/paint.h"
+#include "os/skia/skia_helpers.h"
 #include "os/skia/skia_surface.h"
 
 #include "include/core/SkTextBlob.h"
@@ -25,10 +26,8 @@ void draw_text(Surface* surface, Font* font,
                const TextAlign textAlign)
 {
   SkPaint skPaint;
-  if (paint) {
-    if (paint->color() != gfx::ColorNone)
-      skPaint.setColor(to_skia(paint->color()));
-  }
+  if (paint)
+    to_skia(*paint, skPaint);
 
   SkFont skFont;                // wrap SkFont with os::Font
   SkTextUtils::Draw(

commit 98506341d68a318f4916a891ab12351a5db05f98

Fix SkiaSurface::drawRect() size when we stroke a rectangle
Skia strokes a rectangle with width=height=1 as a 2x2 rectangle
instead of 1x1 (this doesn't happen when we use the kFill_Style). But
we were painting (stroke) rectangles of 1x1 as a pixel. So here we fix
this regression introduced in dd9f578f924f1f8901920b3999350bfe2aaee29a,
related to https://github.com/aseprite/aseprite/issues/2294

os/skia/skia_helpers.h
@@ -1,5 +1,5 @@
 // LAF OS Library
-// Copyright (c) 2019  Igara Studio S.A.
+// Copyright (C) 2019-2020  Igara Studio S.A.
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -16,6 +16,8 @@
 #include "SkPaint.h"
 #include "SkRect.h"
 
+#include <algorithm>
+
 namespace os {
 
 inline SkColor to_skia(gfx::Color c) {
@@ -28,6 +30,12 @@ inline SkIRect to_skia(const gfx::Rect& rc) {
 
 inline SkRect to_skia(const gfx::RectF& rc) {
   return SkRect::MakeXYWH(rc.x, rc.y, rc.w, rc.h);
+}
+
+inline SkRect to_skia_fix(const gfx::RectF& rc) {
+  return SkRect::MakeXYWH(rc.x, rc.y,
+                          std::max(0.0, rc.w-1),
+                          std::max(0.0, rc.h-1));
 }
 
 inline void to_skia(const Paint& paint, SkPaint& skPaint) {

os/skia/skia_surface.cpp
@@ -33,8 +33,16 @@ void SkiaSurface::drawLine(const float x0, const float y0,
 void SkiaSurface::drawRect(const gfx::RectF& rc,
                            const Paint& paint)
 {
+  if (rc.isEmpty())
+    return;
+
   to_skia(paint, m_paint);
-  m_canvas->drawRect(to_skia(rc), m_paint);
+  if (paint.style() == Paint::Style::Stroke) {
+    m_canvas->drawRect(to_skia_fix(rc), m_paint);
+  }
+  else {
+    m_canvas->drawRect(to_skia(rc), m_paint);
+  }
 }
 
 void SkiaSurface::drawCircle(const float cx, const float cy,