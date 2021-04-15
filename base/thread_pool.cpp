f4f841cc05502a014e857b8bf3f9b0b280cf9a60
David Capello <david@igarastudio.com>
2021-04-15 12:01:26 -0300

    Remove unnecessary std::move()

--- base/thread_pool.cpp
+++ base/thread_pool.cpp
@@ -1,5 +1,5 @@
 // LAF Base Library
-// Copyright (C) 2019  Igara Studio S.A.
+// Copyright (C) 2019-2021  Igara Studio S.A.
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -21,7 +21,7 @@ thread_pool::thread_pool(const size_t n)
 {
   std::unique_lock<std::mutex> lock(m_mutex);
   for (size_t i=0; i<n; ++i)
-    m_threads.push_back(std::move(std::thread([this]{ worker(); })));
+    m_threads[i] = std::thread([this]{ worker(); });
 }
 
 thread_pool::~thread_pool()
