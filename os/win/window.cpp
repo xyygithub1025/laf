xyygithub1025/laf

commit 880c733a99bf6bb194cf598438aaf54ee5d0e9fc

[win] Fix issue with Snipping Tool
Using the Snipping Tool sent the window to the back, losing the
focus/active window status. It looks like we cannot change the
WS_VISIBLE style at this point. So to avoid drawing the frame on a
WM_NCACTIVE message, we can temporarily remove the WS_*SCROLL and
WS_THICKFRAME styles.

os/win/window.cpp
@@ -1022,8 +1022,17 @@ LRESULT WindowWin::wndProc(UINT msg, WPARAM wparam, LPARAM lparam)
         // resize grip, a "simple" hack is setting the window as
         // temporarily hidden, and then restoring the style again.
         LONG style = GetWindowLong(m_hwnd, GWL_STYLE);
-        SetWindowLong(m_hwnd, GWL_STYLE, style & ~WS_VISIBLE);
-        auto res = DefWindowProc(m_hwnd, msg, wparam, -1);
+
+        // Remove these styles to avoid drawing the resize grip at the
+        // bottom-right border of the window.
+        style &= ~(WS_HSCROLL | WS_VSCROLL);
+        if (m_borderless) {
+          // Avoid drawing the old-looking frame of the window.
+          style &= ~WS_THICKFRAME;
+        }
+
+        SetWindowLong(m_hwnd, GWL_STYLE, style);
+        auto res = DefWindowProc(m_hwnd, msg, wparam, lparam);
         SetWindowLong(m_hwnd, GWL_STYLE, style);
         return res;
       }

commit e2a3838dc51294a25021e3ea3afcca14c295e904

[win] Restore HWND style in WM_NCACTIVATE
Fixed error introduced in 880c733a99bf6bb194cf598438aaf54ee5d0e9fc

os/win/window.cpp
@@ -1032,7 +1032,8 @@ LRESULT WindowWin::wndProc(UINT msg, WPARAM wparam, LPARAM lparam)
         // DefWindowProc() will try to draw the whole frame + the
         // resize grip, a "simple" hack is setting the window as
         // temporarily hidden, and then restoring the style again.
-        LONG style = GetWindowLong(m_hwnd, GWL_STYLE);
+        LONG oldStyle, style;
+        oldStyle = style = GetWindowLong(m_hwnd, GWL_STYLE);
 
         // Remove these styles to avoid drawing the resize grip at the
         // bottom-right border of the window.
@@ -1044,7 +1045,7 @@ LRESULT WindowWin::wndProc(UINT msg, WPARAM wparam, LPARAM lparam)
 
         SetWindowLong(m_hwnd, GWL_STYLE, style);
         auto res = DefWindowProc(m_hwnd, msg, wparam, lparam);
-        SetWindowLong(m_hwnd, GWL_STYLE, style);
+        SetWindowLong(m_hwnd, GWL_STYLE, oldStyle);
         return res;
       }
       break;