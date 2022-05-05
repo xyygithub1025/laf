c8330a62fe3fba72c5a5094304f80808ee2de5a5
David Capello <david@igarastudio.com>
2022-05-05 14:15:13 -0300

    Mark NativeCursor::Cursors as not needed in switch/cases

--- os/native_cursor.h
+++ os/native_cursor.h
@@ -1,5 +1,5 @@
 // LAF OS Library
-// Copyright (C) 2021  Igara Studio S.A.
+// Copyright (C) 2021-2022  Igara Studio S.A.
 // Copyright (C) 2012-2014  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -34,7 +34,7 @@ namespace os {
     SizeW,
     SizeNW,
 
-    Cursors
+    Cursors [[maybe_unused]]
   };
 
 } // namespace os
