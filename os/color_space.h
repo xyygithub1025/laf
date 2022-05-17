8b0422877a39d655bf6a405eb2e2985763b150da
David Capello <david@igarastudio.com>
2022-05-17 13:13:42 -0300

    Add os::ColorSpace::isSRGB() function

--- os/color_space.h
+++ os/color_space.h
@@ -22,6 +22,7 @@ namespace os {
   public:
     virtual ~ColorSpace() { }
     virtual const gfx::ColorSpaceRef& gfxColorSpace() const = 0;
+    virtual const bool isSRGB() const = 0;
   };
 
   class ColorSpaceConversion : public RefCount {
--- os/skia/skia_color_space.h
+++ os/skia/skia_color_space.h
@@ -22,6 +22,8 @@ public:
   const gfx::ColorSpaceRef& gfxColorSpace() const override { return m_gfxcs; }
   sk_sp<SkColorSpace> skColorSpace() const { return m_skcs; }
 
+  const bool isSRGB() const override { return m_skcs->isSRGB(); }
+
 private:
   gfx::ColorSpaceRef m_gfxcs;
   sk_sp<SkColorSpace> m_skcs;
