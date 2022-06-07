xyygithub1025/laf

commit 12c933a21a2d87c7b0c76a9d5a94314f7e87cd0d

Replace utf8 iterators with utf8_decode in draw_text() functions

ft/hb_shaper.h
@@ -1,5 +1,5 @@
 // LAF FreeType Wrapper
-// Copyright (c) 2020 Igara Studio S.A.
+// Copyright (c) 2020-2022 Igara Studio S.A.
 // Copyright (c) 2017 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -9,6 +9,7 @@
 #define FT_HB_SHAPER_H_INCLUDED
 #pragma once
 
+#include "base/utf8_decode.h"
 #include "ft/hb_face.h"
 
 #include <vector>
@@ -18,24 +19,27 @@ namespace ft {
   template<typename HBFace>
   class HBShaper {
   public:
-    HBShaper(HBFace& face) : m_face(face) {
-    }
 
-    bool initialize(const base::utf8_const_iterator& begin,
-                    const base::utf8_const_iterator& end) {
-      m_index = 0;
-      m_glyphCount = 0;
-      if (begin == end)
-        return false;
-
+    HBShaper(HBFace& face, const std::string& str)
+      : m_face(face) {
       hb_buffer_t* buf = hb_buffer_create();
       hb_buffer_t* chrBuf = hb_buffer_create();
       hb_script_t script = HB_SCRIPT_UNKNOWN;
 
-      for (auto it=begin; it!=end; ++it) {
+      base::utf8_decode decode(str);
+      if (decode.is_end())
+        return;
+
+      const auto begin = str.begin();
+      while (true) {
+        const auto pos = decode.pos();
+        const int chr = decode.next();
+        if (!chr)
+          break;
+
         // Get the script of the next character in *it
         hb_buffer_set_content_type(chrBuf, HB_BUFFER_CONTENT_TYPE_UNICODE);
-        hb_buffer_add(chrBuf, *it, 0);
+        hb_buffer_add(chrBuf, chr, 0);
         hb_buffer_guess_segment_properties(chrBuf);
         hb_script_t newScript = hb_buffer_get_script(chrBuf);
         hb_buffer_reset(chrBuf);
@@ -46,26 +50,26 @@ namespace ft {
           script = newScript;
         }
 
-        hb_buffer_add(buf, *it, it - begin);
+        hb_buffer_add(buf, chr, pos - begin);
       }
       addBuffer(buf, script);
 
       hb_buffer_destroy(buf);
       hb_buffer_destroy(chrBuf);
-
-      return (m_glyphCount > 0);
     }
 
-    bool nextChar() {
-      ++m_index;
-      return (m_index < m_glyphCount);
+    int next() {
+      if (++m_index < m_glyphCount)
+        return m_glyphInfo[m_index].codepoint;
+      else
+        return 0;
     }
 
     int unicodeChar() const {
       return m_glyphInfo[m_index].codepoint;
     }
 
-    int charIndex() {
+    int charIndex() const {
       return m_glyphInfo[m_index].cluster;
     }
 
@@ -115,8 +119,8 @@ namespace ft {
     HBFace& m_face;
     std::vector<hb_glyph_info_t> m_glyphInfo;
     std::vector<hb_glyph_position_t> m_glyphPos;
-    unsigned int m_glyphCount;
-    unsigned int m_index;
+    int m_glyphCount = 0;
+    int m_index = -1;
   };
 
 } // namespace ft

base/utf8_decode.h
@@ -27,6 +27,10 @@ namespace base {
       , m_end(str.end()) {
     }
 
+    iterator pos() const {
+      return m_it;
+    }
+
     bool is_end() const {
       return m_it == m_end;
     }

examples/complextextlayout.cpp
@@ -1,5 +1,5 @@
 // LAF Library
-// Copyright (c) 2019-2021  Igara Studio S.A.
+// Copyright (c) 2019-2022  Igara Studio S.A.
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -59,10 +59,8 @@ void draw_window(os::Window* window,
   gfx::Point pos(0, 0);
   for (auto line : lines) {
     std::string s = base::to_utf8(line);
-    base::utf8_const utf8(s);
     os::draw_text(
-      backSurface.get(), font.get(),
-      utf8.begin(), utf8.end(),
+      backSurface.get(), font.get(), s,
       gfx::rgba(255, 255, 255), gfx::ColorNone,
       pos.x, pos.y,
       &delegate);

ft/algorithm.h
@@ -1,4 +1,5 @@
 // LAF FreeType Wrapper
+// Copyright (c) 2022 Igara Studio S.A.
 // Copyright (c) 2016-2017 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -9,6 +10,7 @@
 #pragma once
 
 #include "base/string.h"
+#include "base/utf8_decode.h"
 #include "ft/freetype_headers.h"
 #include "ft/hb_shaper.h"
 #include "gfx/rect.h"
@@ -18,29 +20,26 @@ namespace ft {
   template<typename FaceFT>
   class DefaultShaper {
   public:
-    typedef typename FaceFT::Glyph Glyph;
+    using Glyph = typename FaceFT::Glyph;
 
-    DefaultShaper(FaceFT& face) : m_face(face) {
+    DefaultShaper(FaceFT& face,
+                  const std::string& str)
+      : m_face(face)
+      , m_begin(str.begin())
+      , m_decode(str) {
     }
 
-    bool initialize(const base::utf8_const_iterator& it,
-                    const base::utf8_const_iterator& end) {
-      m_begin = m_it = it;
-      m_end = end;
-      return (m_it != end);
-    }
-
-    bool nextChar() {
-      ++m_it;
-      return (m_it != m_end);
+    int next() {
+      m_pos = m_decode.pos();
+      return (m_char = m_decode.next());
     }
 
     int unicodeChar() const {
-      return *m_it;
+      return m_char;
     }
 
-    int charIndex() {
-      return m_it - m_begin;
+    int charIndex() const {
+      return m_pos - m_begin;
     }
 
     unsigned int glyphIndex() {
@@ -58,7 +57,10 @@ namespace ft {
 
   private:
     FaceFT& m_face;
-    base::utf8_const_iterator m_begin, m_end, m_it;
+    std::string::const_iterator m_begin;
+    std::string::const_iterator m_pos;
+    base::utf8_decode m_decode;
+    int m_char = 0;
   };
 
   template<typename FaceFT,
@@ -67,9 +69,9 @@ namespace ft {
   public:
     typedef typename FaceFT::Glyph Glyph;
 
-    ForEachGlyph(FaceFT& face)
+    ForEachGlyph(FaceFT& face, const std::string& str)
       : m_face(face)
-      , m_shaper(face)
+      , m_shaper(face, str)
       , m_glyph(nullptr)
       , m_useKerning(FT_HAS_KERNING(((FT_Face)face)) ? true: false)
       , m_prevGlyph(0)
@@ -85,23 +87,16 @@ namespace ft {
 
     const Glyph* glyph() const { return m_glyph; }
 
-    bool initialize(const base::utf8_const_iterator& it,
-                    const base::utf8_const_iterator& end) {
-      bool res = m_shaper.initialize(it, end);
-      if (res)
-        prepareGlyph();
-      return res;
-    }
+    int next() {
+      if (m_glyph)
+        m_prevGlyph = m_shaper.glyphIndex();
 
-    bool nextChar() {
-      m_prevGlyph = m_shaper.glyphIndex();
-
-      if (m_shaper.nextChar()) {
+      if (int chr = m_shaper.next()) {
         prepareGlyph();
-        return true;
+        return chr;
       }
       else
-        return false;
+        return 0;
     }
 
   private:
@@ -156,16 +151,13 @@ namespace ft {
   template<typename FaceFT>
   gfx::Rect calc_text_bounds(FaceFT& face, const std::string& str) {
     gfx::Rect bounds(0, 0, 0, 0);
-    ForEachGlyph<FaceFT> feg(face);
-    if (feg.initialize(base::utf8_const_iterator(str.begin()),
-                       base::utf8_const_iterator(str.end()))) {
-      do {
-        if (auto glyph = feg.glyph())
-          bounds |= gfx::Rect(int(glyph->x),
-                              int(glyph->y),
-                              glyph->bitmap->width,
-                              glyph->bitmap->rows);
-      } while (feg.nextChar());
+    ForEachGlyph<FaceFT> feg(face, str);
+    while (feg.next()) {
+      if (auto glyph = feg.glyph())
+        bounds |= gfx::Rect(int(glyph->x),
+                            int(glyph->y),
+                            glyph->bitmap->width,
+                            glyph->bitmap->rows);
     }
     return bounds;
   }

os/draw_text.cpp
@@ -1,5 +1,5 @@
 // LAF OS Library
-// Copyright (C) 2020  Igara Studio S.A.
+// Copyright (C) 2020-2022  Igara Studio S.A.
 // Copyright (C) 2017  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -21,13 +21,12 @@
 namespace os {
 
 gfx::Rect draw_text(Surface* surface, Font* font,
-                    const base::utf8_const_iterator& begin,
-                    const base::utf8_const_iterator& end,
+                    const std::string& text,
                     gfx::Color fg, gfx::Color bg,
                     int x, int y,
                     DrawTextDelegate* delegate)
 {
-  base::utf8_const_iterator it = begin;
+  base::utf8_decode decode(text);
   gfx::Rect textBounds;
 
 retry:;
@@ -36,8 +35,7 @@ retry:;
   if (font->fallback()) {
     // TODO compose unicode characters and check those codepoints, the
     //      same in the drawing code of sprite sheet font
-    for (auto it=begin; it!=end; ++it) {
-      uint32_t code = *it;
+    while (uint32_t code = decode.next()) {
       if (code && !font->hasCodePoint(code)) {
         Font* newFont = font->fallback();
 
@@ -70,15 +68,18 @@ retry:;
         surface->lock();
       }
 
-      while (it != end) {
-        int chr = *it;
+      decode = base::utf8_decode(text);
+      while (true) {
+        const int i = decode.pos() - text.begin();
+        const int chr = decode.next();
+        if (!chr)
+          break;
+
         gfx::Rect charBounds = ssFont->getCharBounds(chr);
         gfx::Rect outCharBounds(x, y, charBounds.w, charBounds.h);
 
-        if (delegate) {
-          int i = it-begin;
+        if (delegate)
           delegate->preProcessChar(i, chr, fg, bg, outCharBounds);
-        }
 
         if (delegate && !delegate->preDrawChar(outCharBounds))
           break;
@@ -93,7 +94,6 @@ retry:;
           delegate->postDrawChar(outCharBounds);
 
         x += charBounds.w;
-        ++it;
       }
 
       if (surface) {
@@ -115,113 +115,111 @@ retry:;
         surface->lock();
       }
 
-      ft::ForEachGlyph<FreeTypeFont::Face> feg(ttFont->face());
-      if (feg.initialize(it, end)) {
-        do {
-          gfx::Rect origDstBounds;
-          auto glyph = feg.glyph();
-          if (glyph)
-            origDstBounds = gfx::Rect(
-              x + int(glyph->startX),
-              y + int(glyph->y),
-              int(glyph->endX) - int(glyph->startX),
-              int(glyph->bitmap->rows) ? int(glyph->bitmap->rows): 1);
+      ft::ForEachGlyph<FreeTypeFont::Face> feg(ttFont->face(), text);
+      while (feg.next()) {
+        gfx::Rect origDstBounds;
+        auto glyph = feg.glyph();
+        if (glyph)
+          origDstBounds = gfx::Rect(
+            x + int(glyph->startX),
+            y + int(glyph->y),
+            int(glyph->endX) - int(glyph->startX),
+            int(glyph->bitmap->rows) ? int(glyph->bitmap->rows): 1);
 
-          if (delegate) {
-            delegate->preProcessChar(
-              feg.charIndex(),
-              feg.unicodeChar(),
-              fg, bg, origDstBounds);
-          }
+        if (delegate) {
+          delegate->preProcessChar(
+            feg.charIndex(),
+            feg.unicodeChar(),
+            fg, bg, origDstBounds);
+        }
 
-          if (!glyph)
-            continue;
+        if (!glyph)
+          continue;
 
-          if (delegate && !delegate->preDrawChar(origDstBounds))
-            break;
+        if (delegate && !delegate->preDrawChar(origDstBounds))
+          break;
 
-          origDstBounds.x = x + int(glyph->x);
-          origDstBounds.w = int(glyph->bitmap->width);
-          origDstBounds.h = int(glyph->bitmap->rows);
+        origDstBounds.x = x + int(glyph->x);
+        origDstBounds.w = int(glyph->bitmap->width);
+        origDstBounds.h = int(glyph->bitmap->rows);
 
-          gfx::Rect dstBounds = origDstBounds;
-          if (surface)
-            dstBounds &= clipBounds;
+        gfx::Rect dstBounds = origDstBounds;
+        if (surface)
+          dstBounds &= clipBounds;
 
-          if (surface && !dstBounds.isEmpty()) {
-            int clippedRows = dstBounds.y - origDstBounds.y;
-            int dst_y = dstBounds.y;
-            int t;
-            for (int v=0; v<dstBounds.h; ++v, ++dst_y) {
-              int bit = 0;
-              const uint8_t* p = glyph->bitmap->buffer
-                + (v+clippedRows)*glyph->bitmap->pitch;
-              int dst_x = dstBounds.x;
-              uint32_t* dst_address =
-                (uint32_t*)surface->getData(dst_x, dst_y);
+        if (surface && !dstBounds.isEmpty()) {
+          int clippedRows = dstBounds.y - origDstBounds.y;
+          int dst_y = dstBounds.y;
+          int t;
+          for (int v=0; v<dstBounds.h; ++v, ++dst_y) {
+            int bit = 0;
+            const uint8_t* p = glyph->bitmap->buffer
+              + (v+clippedRows)*glyph->bitmap->pitch;
+            int dst_x = dstBounds.x;
+            uint32_t* dst_address =
+              (uint32_t*)surface->getData(dst_x, dst_y);
 
-              // Skip first clipped pixels
-              for (int u=0; u<dstBounds.x-origDstBounds.x; ++u) {
-                if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
+            // Skip first clipped pixels
+            for (int u=0; u<dstBounds.x-origDstBounds.x; ++u) {
+              if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
+                ++p;
+              }
+              else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
+                if (bit == 8) {
+                  bit = 0;
                   ++p;
                 }
-                else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
-                  if (bit == 8) {
-                    bit = 0;
-                    ++p;
-                  }
-                }
-              }
-
-              for (int u=0; u<dstBounds.w; ++u, ++dst_x) {
-                ASSERT(clipBounds.contains(gfx::Point(dst_x, dst_y)));
-
-                int alpha;
-                if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
-                  alpha = *(p++);
-                }
-                else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
-                  alpha = ((*p) & (1 << (7 - (bit++))) ? 255: 0);
-                  if (bit == 8) {
-                    bit = 0;
-                    ++p;
-                  }
-                }
-
-                uint32_t backdrop = *dst_address;
-                gfx::Color backdropColor =
-                  gfx::rgba(
-                    ((backdrop & fd.redMask) >> fd.redShift),
-                    ((backdrop & fd.greenMask) >> fd.greenShift),
-                    ((backdrop & fd.blueMask) >> fd.blueShift),
-                    ((backdrop & fd.alphaMask) >> fd.alphaShift));
-
-                gfx::Color output = gfx::rgba(gfx::getr(fg),
-                                              gfx::getg(fg),
-                                              gfx::getb(fg),
-                                              MUL_UN8(fg_alpha, alpha, t));
-                if (gfx::geta(bg) > 0)
-                  output = blend(blend(backdropColor, bg), output);
-                else
-                  output = blend(backdropColor, output);
-
-                *dst_address =
-                  ((gfx::getr(output) << fd.redShift  ) & fd.redMask  ) |
-                  ((gfx::getg(output) << fd.greenShift) & fd.greenMask) |
-                  ((gfx::getb(output) << fd.blueShift ) & fd.blueMask ) |
-                  ((gfx::geta(output) << fd.alphaShift) & fd.alphaMask);
-
-                ++dst_address;
               }
             }
-          }
 
-          if (!origDstBounds.w) origDstBounds.w = 1;
-          if (!origDstBounds.h) origDstBounds.h = 1;
-          textBounds |= origDstBounds;
-          if (delegate)
-            delegate->postDrawChar(origDstBounds);
-        } while (feg.nextChar());
+            for (int u=0; u<dstBounds.w; ++u, ++dst_x) {
+              ASSERT(clipBounds.contains(gfx::Point(dst_x, dst_y)));
+
+              int alpha;
+              if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_GRAY) {
+                alpha = *(p++);
+              }
+              else if (glyph->bitmap->pixel_mode == FT_PIXEL_MODE_MONO) {
+                alpha = ((*p) & (1 << (7 - (bit++))) ? 255: 0);
+                if (bit == 8) {
+                  bit = 0;
+                  ++p;
+                }
+              }
+
+              uint32_t backdrop = *dst_address;
+              gfx::Color backdropColor =
+                gfx::rgba(
+                  ((backdrop & fd.redMask) >> fd.redShift),
+                  ((backdrop & fd.greenMask) >> fd.greenShift),
+                  ((backdrop & fd.blueMask) >> fd.blueShift),
+                  ((backdrop & fd.alphaMask) >> fd.alphaShift));
+
+              gfx::Color output = gfx::rgba(gfx::getr(fg),
+                                            gfx::getg(fg),
+                                            gfx::getb(fg),
+                                            MUL_UN8(fg_alpha, alpha, t));
+              if (gfx::geta(bg) > 0)
+                output = blend(blend(backdropColor, bg), output);
+              else
+                output = blend(backdropColor, output);
+
+              *dst_address =
+                ((gfx::getr(output) << fd.redShift  ) & fd.redMask  ) |
+                ((gfx::getg(output) << fd.greenShift) & fd.greenMask) |
+                ((gfx::getb(output) << fd.blueShift ) & fd.blueMask ) |
+                ((gfx::geta(output) << fd.alphaShift) & fd.alphaMask);
+
+              ++dst_address;
+            }
+          }
+        }
+
+        if (!origDstBounds.w) origDstBounds.w = 1;
+        if (!origDstBounds.h) origDstBounds.h = 1;
+        textBounds |= origDstBounds;
+        if (delegate)
+          delegate->postDrawChar(origDstBounds);
       }
 
       if (surface)

os/draw_text.h
@@ -1,4 +1,5 @@
 // LAF OS Library
+// Copyright (c) 2022  Igara Studio S.A.
 // Copyright (C) 2017  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -49,8 +50,7 @@ namespace os {
   // (right-to-left) languages.
   gfx::Rect draw_text(
     Surface* surface, Font* font,
-    const base::utf8_const_iterator& begin,
-    const base::utf8_const_iterator& end,
+    const std::string& text,
     gfx::Color fg, gfx::Color bg,
     int x, int y,
     DrawTextDelegate* delegate);

commit c572dca743178f74fad3f916e0cf8bb856177df9

Fix memory leak drawing empty strings w/FT & HBShaper (introduced in 12c933a21a2d87c7b0c76a9d5a94314f7e87cd0d)

ft/hb_shaper.h
@@ -22,13 +22,13 @@ namespace ft {
 
     HBShaper(HBFace& face, const std::string& str)
       : m_face(face) {
-      hb_buffer_t* buf = hb_buffer_create();
-      hb_buffer_t* chrBuf = hb_buffer_create();
-      hb_script_t script = HB_SCRIPT_UNKNOWN;
-
       base::utf8_decode decode(str);
       if (decode.is_end())
         return;
+
+      hb_buffer_t* buf = hb_buffer_create();
+      hb_buffer_t* chrBuf = hb_buffer_create();
+      hb_script_t script = HB_SCRIPT_UNKNOWN;
 
       const auto begin = str.begin();
       while (true) {