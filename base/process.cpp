a6c13ab3c349cf76232c7d048afb051cf2a82537
Gaspar Capello <gaspar@igara.com>
2023-12-26 16:56:23 -0300

    New 'get_process_name()' function (Windows + macOS impl)
    This function is necessary for:
    https://github.com/aseprite/aseprite/issues/4130#issuecomment-1866333196

--- base/process.cpp
+++ base/process.cpp
@@ -1,5 +1,5 @@
 // LAF Base Library
-// Copyright (c) 2021 Igara Studio S.A.
+// Copyright (c) 2021-2024 Igara Studio S.A.
 // Copyright (c) 2015-2016 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -11,12 +11,19 @@
 
 #include "base/process.h"
 
+#include "base/string.h"
+
 #if LAF_WINDOWS
   #include <windows.h>
+  #include <tlhelp32.h>
 #else
   #include <signal.h>
   #include <sys/types.h>
   #include <unistd.h>
+#endif
+
+#if LAF_MACOS
+  #include <libproc.h>
 #endif
 
 namespace base {
@@ -44,7 +51,25 @@ bool is_process_running(pid pid)
   return running;
 }
 
-#else // !LAF_WINDOWS
+std::string get_process_name(pid pid)
+{
+  bool running = false;
+  HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+  if (handle) {
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(PROCESSENTRY32);
+    if (Process32First(handle, &pe)) {
+      do {
+        if (pe.th32ProcessID == pid)
+          return base::to_utf8(pe.szExeFile);
+      } while (Process32Next(handle, &pe));
+    }
+    CloseHandle(handle);
+  }
+  return std::string();
+}
+
+#else
 
 pid get_current_process_id()
 {
@@ -56,6 +81,26 @@ bool is_process_running(pid pid)
   return (kill(pid, 0) == 0);
 }
 
+#if LAF_MACOS
+
+std::string get_process_name(pid pid)
+{
+  struct proc_bsdinfo process;
+  proc_pidinfo(pid, PROC_PIDTBSDINFO, 0,
+               &process, PROC_PIDTBSDINFO_SIZE);
+  return process.pbi_name;
+}
+
+#else
+
+std::string get_process_name(pid pid)
+{
+  // TODO implement for Linux
+  return std::string();
+}
+
 #endif
+
+#endif  // LAF_WINDOWS
 
 } // namespace base
--- base/process.h
+++ base/process.h
@@ -1,4 +1,5 @@
 // LAF Base Library
+// Copyright (c) 2023-2024 Igara Studio S.A.
 // Copyright (c) 2015-2016 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -10,6 +11,8 @@
 
 #include "base/ints.h"
 
+#include <string>
+
 namespace base {
 
   typedef uint32_t pid;
@@ -17,6 +20,8 @@ namespace base {
   pid get_current_process_id();
 
   bool is_process_running(pid pid);
+
+  std::string get_process_name(pid pid);
 
 } // namespace base
 
